8d6ebe46fdeae237cb4d26b7b09eab5d4b973d92
parsazn <0619904874@uma.es>
2021-04-15 21:10:52 +0200

    just replies with a simple Hello

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world \n")
+import telebot
+import time
+
+bot_token='1584434812:AAFocYO8lD9W-6th7B2Pb1PhvByDtrtO9pQ'
+bot = telebot.TeleBot(token=bot_token)
+
+@bot.message_handler(commands=['start'])
+def send_welcome (message):
+    bot.reply_to(message,'welcome!')
+
+bot.polling()
